c7141582b557b20d0d1156656a0aadea654c69cf
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-06 05:10:22 -0500

    Initial Project Setup

---
+++ src/main/java/com/morris_je/mwdspringpetclinic/MwdSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.morris_je.mwdspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwdSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwdSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morris_je/mwdspringpetclinic/MwdSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morris_je.mwdspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwdSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
