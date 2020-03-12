bfec279436459aff61531d9b862ee6b0335efc2b
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-12 07:03:24 -0400

    Added index page and controller closes #7

---
+++ pet-clinic-web/src/main/java/com/morris_je/mwdspringpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.morris_je.mwdspringpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
