0638bc75c7719e14a59e8502a915bac0f154dd10
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-12 03:51:40 -0400

    Added BaseEntity to extract the Id value. Closes #11.

--- pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.morris_je.mwdspringpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.morris_je.mwdspringpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
 
