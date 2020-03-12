732081de21525f6f0eb90b12cb77759b3dc29570
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-12 06:19:07 -0400

    Added Map based services. closes #6.

---
+++ pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/services/map/PetMapService.java
@@ -0,0 +1,33 @@
+package com.morris_je.mwdspringpetclinic.services.map;
+
+import com.morris_je.mwdspringpetclinic.model.Pet;
+import com.morris_je.mwdspringpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
