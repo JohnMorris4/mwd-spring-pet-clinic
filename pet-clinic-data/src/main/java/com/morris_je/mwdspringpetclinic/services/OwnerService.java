356fe3d306bd6bfe36e146d6b7b618bb9458931a
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-12 05:39:02 -0400

    Fixed the duplicated code by adding JAVA Generics in the CrudService

--- pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/services/OwnerService.java
@@ -2,15 +2,6 @@ package com.morris_je.mwdspringpetclinic.services;
 
 import com.morris_je.mwdspringpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 }
--- pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/morris_je/mwdspringpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package com.morris_je.mwdspringpetclinic.services;
 
 import com.morris_je.mwdspringpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
