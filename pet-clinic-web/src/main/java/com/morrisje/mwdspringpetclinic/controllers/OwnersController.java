1957533e1c85f81595084ddd202b411b6ac61b5c
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-18 18:58:06 -0400

    Created the stubs for the vet and customer pages also fixed the DataLoader. closes #16 and #17

--- pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/OwnersController.java
@@ -1,12 +1,23 @@
 package com.morrisje.mwdspringpetclinic.controllers;
 
+import com.morrisje.mwdspringpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnersController {
+
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index"})
-    public String index(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/VetsController.java
@@ -1,12 +1,22 @@
 package com.morrisje.mwdspringpetclinic.controllers;
 
+import com.morrisje.mwdspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html"})
-    public String index() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
 
     }
