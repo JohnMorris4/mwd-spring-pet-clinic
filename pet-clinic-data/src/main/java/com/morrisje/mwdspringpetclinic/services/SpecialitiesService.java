9699bf601db8556ba76ee0b757a234bfd6cb8520
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-20 03:12:17 -0400

    Created the SpecialitiesService and SpecialityMapService. closes #32

---
+++ pet-clinic-data/src/main/java/com/morrisje/mwdspringpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.morrisje.mwdspringpetclinic.services;
+
+import com.morrisje.mwdspringpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
