6a3a61a515e63165ba485e049899804984173028
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-19 21:50:51 -0400

    Implemented the PetTypeMapService and the PetTypeService. closes #30

---
+++ pet-clinic-data/src/main/java/com/morrisje/mwdspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.morrisje.mwdspringpetclinic.services;
+
+import com.morrisje.mwdspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
