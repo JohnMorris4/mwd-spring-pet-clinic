19e1100e5e57a1b7e8e3a0bea00eda5828a734ff
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-20 04:48:01 -0400

    Setup the error view pages and linked to the controllers. closes #46

--- pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/OwnersController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
@@ -14,10 +15,15 @@ public class OwnersController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners", "/owners/index"})
+    @RequestMapping({ "" ,"/index", "/index.html"})
     public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping({"/find"})
+    public String findOwners(){
+        return "notimplemented";
     }
 }
