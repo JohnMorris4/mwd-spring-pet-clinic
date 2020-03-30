d8b26f89788b0afdae9cc580adde488fb1d21dc7
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-30 06:08:07 -0400

    Added the Display Owners.  App is broken. closes #54

--- pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/morrisje/mwdspringpetclinic/controllers/OwnersController.java
@@ -3,13 +3,17 @@ package com.morrisje.mwdspringpetclinic.controllers;
 import com.morrisje.mwdspringpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
     private final OwnerService ownerService;
+    //private  OwnerRepository owners;
 
     public OwnersController(OwnerService ownerService) {
         this.ownerService = ownerService;
@@ -25,5 +29,12 @@ public class OwnersController {
     @RequestMapping({"/find"})
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
